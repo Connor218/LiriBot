122d8c260b779724ef3307e3f32b379311d5d9af
Admin <admin@Admins-MacBook-Pro.local>
2018-05-21 23:57:14 -0700

    add files

--- app.js
+++ app.js
@@ -37,12 +37,7 @@ request(queryUrl, function(error, response, body) {
 //Twitter API 
 var Twitter = require('twitter');
  
-var client = new Twitter({
-  consumer_key: 'yQ9b9jAqTBY3CAiwE0NrIQaBB',
-  consumer_secret: 'VaBrajlMTdeu0DD0eGwkBbV5GVNvWd1KseGhMXKRP1OMXBJmDl',
-  access_token_key: '555614925-VUtSJHi4x4BruQR25FEeqwxWdNjFLQH2HxRdOgQG',
-  access_token_secret: 'Qke2w7sZ1DJACeXOb2GF4IqczhG6ztt0EsZXyJOybDqLO'
-});
+var client = new Twitter(keys.twitter);
  
 var params = {screen_name: 'nodejs'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
@@ -58,10 +53,7 @@ var Spotify = require('node-spotify-api');
 var search = function({ type= 'artist OR album OR track', query= 'My search query', limit= 20 }, callback){
 
  
-var spotify = new Spotify({
-  id: e22512ca96c84af0be17aafb3572cbf0,
-  secret: fe997f4d1cce4e2dbbea26e53010d9a7,
-  });
+var spotify = new Spotify(keys.spotify);
  
 spotify.search(type='track', query='All the Small Things', function(err, data) {
   if (err) {
---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.yQ9b9jAqTBY3CAiwE0NrIQaBB,
+  consumer_secret: process.env.VaBrajlMTdeu0DD0eGwkBbV5GVNvWd1KseGhMXKRP1OMXBJmDl,
+  access_token_key: process.env, 555614925:VUtSJHi4x4BruQR25FEeqwxWdNjFLQH2HxRdOgQG,
+  access_token_secret: process.env.Qke2w7sZ1DJACeXOb2GF4IqczhG6ztt0EsZXyJOybDqLO
+};
+
+exports.spotify = {
+  id: process.env.e22512ca96c84af0be17aafb3572cbf0,
+  secret: process.env.fe997f4d1cce4e2dbbea26e53010d9a7
+};

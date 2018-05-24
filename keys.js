b4c0b7e142be807bd672d0475731ecd790cdf7e0
Admin <admin@calvisitor-10-105-167-138.calvisitor.1918.berkeley.edu>
2018-05-24 10:10:22 -0700

    fix js

--- keys.js
+++ keys.js
@@ -1,13 +1,11 @@
-console.log('this is loaded');
-
 exports.twitter = {
-  consumer_key: process.env.yQ9b9jAqTBY3CAiwE0NrIQaBB,
-  consumer_secret: process.env.VaBrajlMTdeu0DD0eGwkBbV5GVNvWd1KseGhMXKRP1OMXBJmDl,
-  access_token_key: process.env, 555614925:VUtSJHi4x4BruQR25FEeqwxWdNjFLQH2HxRdOgQG,
-  access_token_secret: process.env.Qke2w7sZ1DJACeXOb2GF4IqczhG6ztt0EsZXyJOybDqLO
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-  id: process.env.e22512ca96c84af0be17aafb3572cbf0,
-  secret: process.env.fe997f4d1cce4e2dbbea26e53010d9a7
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -1,1 +1,95 @@
 require("dotenv").config();
+var keys = require('./keys.js');
+var inquirer = require('inquirer');
+//omdb Request
+
+var request = require("request");
+
+// var nodeArgs = process.argv;
+var movieName = "";
+
+// for (var i = 2; i < nodeArgs.length; i++) {
+
+//   if (i > 2 && i < nodeArgs.length) {
+
+//     movieName = movieName + "+" + nodeArgs[i];
+
+//   }
+
+//   else {
+
+//     movieName += nodeArgs[i];
+
+//   }
+// }
+function liri(){
+
+    inquirer.prompt([{
+        type:'list',
+        name:'choices',
+        message:'What you want?',
+        choices:['Twitter','Spotify','Movies','Do what it says']
+    }]).then(function(x){
+        switch(x.choices) {
+            case "Twitter":
+            twitter()
+            break;
+           
+            case "Spotify":
+            spotify()
+            break;
+
+        }
+
+    })
+
+
+
+};
+
+
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+console.log(queryUrl);
+
+request(queryUrl, function(error, response, body) {
+
+  if (!error && response.statusCode === 200) {
+
+    console.log("Release Year: " + JSON.parse(body).Year);
+  }
+});
+
+
+
+//Twitter API 
+var Twitter = require('twitter');
+ 
+var client = new Twitter(keys.twitter);
+ 
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
+
+//Spotify API 
+var Spotify = require('node-spotify-api');
+
+var search = function({ type= 'artist OR album OR track', query= 'My search query', limit= 20 }, callback){
+
+ 
+var spotify = new Spotify(keys.spotify);
+ 
+spotify.search(type='track', query='All the Small Things', function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
+
+};
+//wat dis?
